d7d65748790c3a9d7361c194bed35fee5ea022a9
Adil <adil.jawaid@outlook.com>
2023-06-28 10:54:29 +0100

    completing activity 5

--- models/articles.models.js
+++ models/articles.models.js
@@ -16,9 +16,8 @@ exports.selectArticleById = (article_id) => {
 exports.selectAllArticles = () => {
     
 
-    return db.query(` SELECT articles.*, COUNT(comments.article_id) AS comment_count
-    FROM articles
-    LEFT JOIN comments ON articles.article_id = comments.article_id
+    return db.query(`SELECT articles.*, COUNT(comments.article_id) AS comment_count
+    FROM articles LEFT JOIN comments ON articles.article_id = comments.article_id
     GROUP BY articles.article_id
     ORDER BY articles.created_at DESC;`)
         .then((articles) => {
