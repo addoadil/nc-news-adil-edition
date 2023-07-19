33a1c6ff76f1905f327f23bd0cfd05a5055fbe93
Adil <adil.jawaid@outlook.com>
2023-07-19 11:31:33 +0100

    Fixing select query for api endpoint api/articles/:articlei_id

--- models/articles.models.js
+++ models/articles.models.js
@@ -1,7 +1,7 @@
 const db = require('../db/connection');
 
 exports.selectArticleById = (article_id) => {
-  return db.query(`SELECT * FROM articles INNER JOIN (SELECT article_id, COUNT(*) AS comment_count FROM comments WHERE article_id = $1 GROUP BY article_id) comment_counts ON articles.article_id = comment_counts.article_id WHERE articles.article_id = $1;`, [article_id])
+  return db.query(`SELECT articles.author, title, articles.article_id, articles.body, topic, articles.created_at, articles.votes, article_img_url, COUNT(comment_id)::INT as comment_count FROM articles LEFT JOIN comments ON articles.article_id = comments.article_id WHERE articles.article_id = $1 GROUP BY articles.article_id`, [article_id])
     .then((rows) => {
       if (!rows.rows.length) {
         return Promise.reject({ status: 404, msg: 'Not found' });
